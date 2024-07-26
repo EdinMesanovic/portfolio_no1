8a4f9ff0d840f49c313bb07cdf4e12cb1fb57c18
EdinMesanovic <edo_tz_@hotmail.com>
2024-07-26 18:42:34 +0200

    change

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <About />
         <Skills />
         <Services />
-        <Qualification />
+        {/* <Qualification /> */}
         {/* <Testimonials /> */}
         <Contact />
     </main>
