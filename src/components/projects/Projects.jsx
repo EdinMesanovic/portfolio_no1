172a5ccd400979fd4ae9cc8c6ed3c4b782a93ded
EdinMesanovic <edo_tz_@hotmail.com>
2024-09-27 21:55:58 +0200

    Projects update

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,6 +3,8 @@ import './projects.css'
 import IcBerlin from '../../assets/ic-berlin.png'
 import TiBih from '../../assets/ti-bih.png'
 import Meridian from '../../assets/meridian-imel.png'
+import Susara from '../../assets/susara.png'
+import SusaraLandingPage from '../../assets/susara-landingpage.png'
 
 const Projects = () => {
   return (
@@ -31,6 +33,20 @@ const Projects = () => {
             <img src={Meridian} alt="Project Image" className="projects__image" />
 
             <a href='https://meridian.imel.cloud/app/login' className="projects__button" target='_blank'>
+              <i className="uil uil-external-link-alt projects__button-icon"></i>
+            </a>
+          </div>
+          <div className="projects__content">
+            <img src={Susara} alt="Project Image" className="projects__image" />
+
+            <a href='https://susara.edinmesan.ba/login' className="projects__button" target='_blank'>
+              <i className="uil uil-external-link-alt projects__button-icon"></i>
+            </a>
+          </div>
+          <div className="projects__content">
+            <img src={SusaraLandingPage} alt="Project Image" className="projects__image" />
+
+            <a href='https://susara-landingpage.edinmesan.ba/' className="projects__button" target='_blank'>
               <i className="uil uil-external-link-alt projects__button-icon"></i>
             </a>
           </div>
