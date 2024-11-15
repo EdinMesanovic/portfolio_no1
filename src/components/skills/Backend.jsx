6c474837a68c44173b9f83224237108a1fd53c0c
EdinMesanovic <edo_tz_@hotmail.com>
2024-11-15 16:37:34 +0100

    Skills update

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -29,6 +29,15 @@ const Backend = () => {
                     <i className="bx bx-badge-check"></i>
 
                     <div>
+                        <h3 className="skills__name">ASP.NET</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+
+                    <div>
                         <h3 className="skills__name">Express</h3>
                         <span className="skills__level">Proficient</span>
                     </div>
@@ -51,6 +60,15 @@ const Backend = () => {
                     <div>
                         <h3 className="skills__name">MySQL</h3>
                         <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+
+                    <div>
+                        <h3 className="skills__name">PostgreSQL</h3>
+                        <span className="skills__level">Beginner</span>
                     </div>
                 </div>
 
